34967023a3e2a2c306c1d3442e357edc47709f77
Trantien03 <115622082+Trantien03@users.noreply.github.com>
2022-12-26 11:11:33 +0700

    code web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule, Routes} from "@angular/router";
 import { AppComponent } from './app.component';
+import {FavoriesComponent} from "./page/favories/favories.component";
 
+const appRoutes: Routes = [
+
+  {path:'noidung1',component:FavoriesComponent},
+
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, FavoriesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
